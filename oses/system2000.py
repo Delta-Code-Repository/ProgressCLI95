86a95b1b7add9cd07b66b64b0482e4e6a09eda0a
Dieguito0512 <98424967+Dieguito0512@users.noreply.github.com>
2022-01-26 11:20:25 -0600

    Progressbar 2000

--- oses/system2000.py
+++ oses/system2000.py
@@ -9,5 +9,5 @@ def launch2000(systemlevel, systembadge, systempro):
     print('P r o g r e s s b a r  2 0 0 0')
     print(systembadge)
     print('\n\n\nNow Loading...')
-    sleep(4)
+    sleep(5)
     beginMenu("2000", systemlevel, systempro)
