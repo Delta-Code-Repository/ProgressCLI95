c60a6abc3e17fc7a6ba44fcf53dc7dd80672cc78
CreateSource <72283721+abnormalhare@users.noreply.github.com>
2022-02-14 09:07:59 -0500

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,2 +1,2 @@
-systemList = ["Progressbar 95","Progressbar 95 Plus","Progressbar 98", "Progressbar 2000", "Progressbar Meme"]
-proList = [10, 20, 20, 30, 30]
+systemList = ["Progressbar 95","Progressbar 95 Plus","Progressbar 98", "Progressbar 2000", "Progressbar Meme", "Progressbar XB"]
+proList = [10, 20, 20, 30, 30, 40]
