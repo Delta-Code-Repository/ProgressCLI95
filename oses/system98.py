527e5f44f21928fd82fe490c8b7b8d60f93f85b2
BurningInfern0 <epiclawl46@gmail.com>
2022-02-15 10:08:40 -0500

    Update system98.py

--- oses/system98.py
+++ oses/system98.py
@@ -4,6 +4,14 @@ from clear import clear
 from time import sleep
 from player import startGame, beginMenu, pauseBeginMenu
 
+global pro98
+pro98 = 20
+unlocklevel = 25
+# required system for code
+requiredsystem = "95plus"
+# required system that shows on the boot menu
+requiredstring = "PB95+"
+
 def launch98(systemlevel, systembadge, systempro):
     clear()
     print('P r o g r e s s b a r  9 8')
