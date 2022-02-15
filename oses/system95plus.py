f8ab1ac5f05fa098f0e41f6244b27f42171293fe
BurningInfern0 <epiclawl46@gmail.com>
2022-02-15 10:08:24 -0500

    Update system95plus.py

--- oses/system95plus.py
+++ oses/system95plus.py
@@ -4,6 +4,14 @@ from clear import clear
 from time import sleep
 from player import startGame, beginMenu, pauseBeginMenu
 
+global pro95plus
+pro95plus = 20
+unlocklevel = 15
+# required system for code
+requiredsystem = "95"
+# required system that shows on the boot menu
+requiredstring = "PB95"
+
 def launch95plus(systemlevel, systembadge, systempro):
     clear()
     print('P r o g r e s s b a r  9 5  p l u s')
