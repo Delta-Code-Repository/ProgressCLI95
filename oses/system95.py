9a3798470846a5c0fad059aae97f46aec1c17b9a
BurningInfern0 <epiclawl46@gmail.com>
2022-02-15 10:08:06 -0500

    Update system95.py

--- oses/system95.py
+++ oses/system95.py
@@ -4,6 +4,12 @@ from clear import clear
 from time import sleep
 from player import startGame, beginMenu, pauseBeginMenu
 
+global pro95
+pro95 = 10
+unlocklevel = 0
+requiredsystem = ""
+requiredstring = ""
+
 def launch95(systemlevel, systembadge, systempro):
     clear()
     print('P r o g r e s s b a r  9 5')
