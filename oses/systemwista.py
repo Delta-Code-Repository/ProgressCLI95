927d83383514e9089da9c281b68d0f528466660e
BurningInfern0 <epiclawl46@gmail.com>
2022-02-15 10:09:50 -0500

    Update systemwista.py

--- oses/systemwista.py
+++ oses/systemwista.py
@@ -4,6 +4,14 @@ from clear import clear
 from time import sleep
 from player import startGame, beginMenu, pauseBeginMenu
 
+global prowista
+prowista = 40
+unlocklevel = 50
+# required system for code
+requiredsystem = "XB"
+# required system that shows on the boot menu
+requiredstring = "PBXB"
+
 # How about some uniqueness to these things? I can just do these by copypasting them.
 # Just a suggestion.
 def launchwista(systemlevel, systembadge, systempro):
