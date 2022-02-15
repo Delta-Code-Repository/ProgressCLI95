7b4c1f68a87bb148a1b74380ec9927e57aeb7874
BurningInfern0 <epiclawl46@gmail.com>
2022-02-15 10:09:18 -0500

    Update system2000.py

--- oses/system2000.py
+++ oses/system2000.py
@@ -4,6 +4,14 @@ from clear import clear
 from time import sleep
 from player import startGame, beginMenu, pauseBeginMenu
 
+global pro2000
+pro2000 = 30
+unlocklevel = 30
+# required system for code
+requiredsystem = "Meme"
+# required system that shows on the boot menu
+requiredstring = "PBMeme"
+
 def launch2000(systemlevel, systembadge, systempro):
     clear()
     print('P r o g r e s s b a r  2 0 0 0')
