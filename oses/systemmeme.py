195aa653adfe7811cc58e2e141bc910b1adfc8cf
BurningInfern0 <epiclawl46@gmail.com>
2022-02-15 10:09:01 -0500

    Update systemmeme.py

--- oses/systemmeme.py
+++ oses/systemmeme.py
@@ -4,6 +4,14 @@ from clear import clear
 from time import sleep
 from player import startGame, beginMenu, pauseBeginMenu
 
+global promeme
+promeme = 30
+unlocklevel = 30
+# required system for code
+requiredsystem = "98"
+# required system that shows on the boot menu
+requiredstring = "PB98"
+
 def launchmeme(systemlevel, systembadge, systempro):
     clear()
     print('P r o g r e s s b a r  M e m e')
