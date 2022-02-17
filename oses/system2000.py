70e9782b2e28bf2a12cb40affbdfaf39bf8240fe
BurningInfern0 <epiclawl46@gmail.com>
2022-02-17 08:48:02 -0500

    Update system2000.py

--- oses/system2000.py
+++ oses/system2000.py
@@ -1,21 +1,6 @@
-import sys
-sys.path.insert(0, '../')
-from clear import clear
-from time import sleep
-from player import startGame, beginMenu, pauseBeginMenu
-
-global pro2000
-pro2000 = 30
-unlocklevel = 30
-# required system for code
+name = "Progressbar 2000"
+shortname = "2000"
+prolevel = 30
+unlocklevel = 35
 requiredsystem = "Meme"
-# required system that shows on the boot menu
 requiredstring = "PBMeme"
-
-def launch2000(systemlevel, systembadge, systempro):
-    clear()
-    print('P r o g r e s s b a r  2 0 0 0')
-    print(systembadge)
-    print('\n\n\nNow Loading...')
-    sleep(5)
-    beginMenu("2000", systemlevel, systempro)
