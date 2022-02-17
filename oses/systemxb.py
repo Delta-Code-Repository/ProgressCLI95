ce87ea878818064b9d460e537c07e8038d47ce0d
BurningInfern0 <epiclawl46@gmail.com>
2022-02-17 08:49:09 -0500

    Update systemxb.py

--- oses/systemxb.py
+++ oses/systemxb.py
@@ -1,21 +1,6 @@
-import sys
-sys.path.insert(0, '../')
-from clear import clear
-from time import sleep
-from player import startGame, beginMenu, pauseBeginMenu
-
-global proxb
-proxb = 40
-unlocklevel = 40
-# required system for code
+name = "Progressbar XB"
+shortname = "XB"
+prolevel = 40
+unlocklevel = 35
 requiredsystem = "2000"
-# required system that shows on the boot menu
 requiredstring = "PB2000"
-
-def launchxb(systemlevel, systembadge, systempro):
-    clear()
-    print('P r o g r e s s b a r  X B')
-    print(systembadge)
-    print('\n\n\nNow Loading...')
-    sleep(5)
-    beginMenu("xb", systemlevel, systempro)
