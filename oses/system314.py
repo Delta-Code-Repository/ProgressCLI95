6fdf7f620bdd98b74c0892a9ea3121b098706417
BurningInfern0 <epiclawl46@gmail.com>
2022-02-17 08:45:39 -0500

    re-re-making the system py files

--- oses/system314.py
+++ oses/system314.py
@@ -1,19 +1,6 @@
-import sys
-sys.path.insert(0, '../')
-from clear import clear
-from time import sleep
-from player import startGame, beginMenu, pauseBeginMenu
-
-global pro314
-pro314 = 10
+name = "Progressbar 3.14"
+shortname = "314"
+prolevel = 10
 unlocklevel = "2p"
 requiredsystem = ""
 requiredstring = ""
-
-def launch95(systemlevel, systembadge, systempro):
-    clear()
-    print('P r o g r e s s b a r  3 . 1 4')
-    print(systembadge)
-    print('\n\n\nNow Loading...')
-    sleep(4)
-    beginMenu("314", systemlevel, systempro)
