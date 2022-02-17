18addf18a1b0e50bed8f86320041c8299f2abe2a
BurningInfern0 <epiclawl46@gmail.com>
2022-02-17 08:46:45 -0500

    Update system95plus.py

--- oses/system95plus.py
+++ oses/system95plus.py
@@ -1,21 +1,6 @@
-import sys
-sys.path.insert(0, '../')
-from clear import clear
-from time import sleep
-from player import startGame, beginMenu, pauseBeginMenu
-
-global pro95plus
-pro95plus = 20
+name = "Progressbar 95 plus"
+shortname = "95plus"
+prolevel = 20
 unlocklevel = 15
-# required system for code
 requiredsystem = "95"
-# required system that shows on the boot menu
 requiredstring = "PB95"
-
-def launch95plus(systemlevel, systembadge, systempro):
-    clear()
-    print('P r o g r e s s b a r  9 5  p l u s')
-    print(systembadge)
-    print('\n\n\nNow Loading...')
-    sleep(4)
-    beginMenu("95plus", systemlevel, systempro)
