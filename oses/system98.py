0642c515d5a22c28c7a031aec3bb423613b1b707
BurningInfern0 <epiclawl46@gmail.com>
2022-02-17 08:47:02 -0500

    Update system98.py

--- oses/system98.py
+++ oses/system98.py
@@ -1,21 +1,6 @@
-import sys
-sys.path.insert(0, '../')
-from clear import clear
-from time import sleep
-from player import startGame, beginMenu, pauseBeginMenu
-
-global pro98
-pro98 = 20
+name = "Progressbar 98"
+shortname = "98"
+prolevel = 20
 unlocklevel = 25
-# required system for code
 requiredsystem = "95plus"
-# required system that shows on the boot menu
 requiredstring = "PB95+"
-
-def launch98(systemlevel, systembadge, systempro):
-    clear()
-    print('P r o g r e s s b a r  9 8')
-    print(systembadge)
-    print('\n\n\nNow Loading...')
-    sleep(4)
-    beginMenu("98", systemlevel, systempro)
