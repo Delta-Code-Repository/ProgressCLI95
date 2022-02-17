5088019bbbdf7c865dd60c2d893bedb2aa535624
BurningInfern0 <epiclawl46@gmail.com>
2022-02-17 08:46:30 -0500

    Update system95.py

--- oses/system95.py
+++ oses/system95.py
@@ -1,19 +1,6 @@
-import sys
-sys.path.insert(0, '../')
-from clear import clear
-from time import sleep
-from player import startGame, beginMenu, pauseBeginMenu
-
-global pro95
-pro95 = 10
+name = "Progressbar 95"
+shortname = "95"
+prolevel = 10
 unlocklevel = 0
 requiredsystem = ""
 requiredstring = ""
-
-def launch95(systemlevel, systembadge, systempro):
-    clear()
-    print('P r o g r e s s b a r  9 5')
-    print(systembadge)
-    print('\n\n\nNow Loading...')
-    sleep(4)
-    beginMenu("95", systemlevel, systempro)
