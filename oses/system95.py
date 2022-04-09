683689b044ed37d20f10b6426b3fefdc462bdecf
Mark <markverb4@gmail.com>
2022-04-09 20:16:47 +0300

    fix

--- oses/system95.py
+++ oses/system95.py
@@ -8,7 +8,7 @@ from player import startGame, beginMenu, pauseBeginMenu
 def launch95(systemlevel, systembadge, systempro, settingsdict):
     clear()
     rprint("\n    [on bright_red] [/]   [on bright_yellow] [/]\n  [on bright_cyan] [/]   [on blue] [/]\n    [on bright_yellow] [/]   [on bright_red] [/]\n  [on blue] [/]   [on bright_cyan] [/]\n\n[on bright_black]           [/]\n[on bright_black] [/][on blue] [/][on bright_yellow] [/][on blue] [/][on bright_yellow] [/]     [bold bright_green on bright_black] [/]\n[on bright_black]           [/]")
-    rprint('[bold]Progressbar[/]  95 [bold][bright_black]+[/bright_black][bright_yellow]P[/bright_yellow][bright_red]L[/bright_red][bright_blue]U[/bright_blue][bright_green]S[/bright_green][/bold]')
+    rprint('[bold]Progressbar[/]  95')
     print(systembadge)
     print('\n\n\nNow Loading...')
     sleep(4)
