33806e1a4983d26bbaf6c4a89b9a8c6cfa30d5db
BurningInfern0 <epiclawl46@gmail.com>
2022-04-24 01:50:35 -0400

    Update system2000.py

--- oses/system2000.py
+++ oses/system2000.py
@@ -1,6 +1,8 @@
-name = "Progressbar 2000"
-shortname = "2000"
-prolevel = 30
-unlocklevel = 35
-requiredsystem = "Meme"
-requiredstring = "PBMeme"
+class system():
+    name = "Progressbar 2000"
+    shortname = "2000"
+    prolevel = 30
+    unlocklevel = 35
+    systemunlock = "xb"
+    requiredstring = "PBMeme"
+    listinbootmenu = 4
