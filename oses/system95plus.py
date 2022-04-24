326b93aff4fde9392b71ed73c0e0ec0490f3938f
BurningInfern0 <epiclawl46@gmail.com>
2022-04-24 01:50:01 -0400

    Update system95plus.py

--- oses/system95plus.py
+++ oses/system95plus.py
@@ -1,6 +1,8 @@
-name = "Progressbar 95 plus"
-shortname = "95plus"
-prolevel = 20
-unlocklevel = 15
-requiredsystem = "95"
-requiredstring = "PB95"
+class system():
+    name = "Progressbar 95 plus"
+    shortname = "95plus"
+    prolevel = 20
+    unlocklevel = 15
+    systemunlock = "98"
+    requiredstring = "PB95"
+    listinbootmenu = 1
