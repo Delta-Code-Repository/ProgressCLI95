252cd59d1984dd5b3229e8320aef9b9751b2b7c2
BurningInfern0 <epiclawl46@gmail.com>
2022-04-24 01:50:12 -0400

    Update system98.py

--- oses/system98.py
+++ oses/system98.py
@@ -1,6 +1,8 @@
-name = "Progressbar 98"
-shortname = "98"
-prolevel = 20
-unlocklevel = 25
-requiredsystem = "95plus"
-requiredstring = "PB95+"
+class system():
+    name = "Progressbar 98"
+    shortname = "98"
+    prolevel = 20
+    unlocklevel = 25
+    systemunlock = "meme"
+    requiredstring = "PB95+"
+    listinbootmenu = 2
