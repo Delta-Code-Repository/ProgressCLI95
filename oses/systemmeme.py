a34aced037f0a47e54acbff2f15d34f3a8639d28
BurningInfern0 <epiclawl46@gmail.com>
2022-04-24 01:50:24 -0400

    Update systemmeme.py

--- oses/systemmeme.py
+++ oses/systemmeme.py
@@ -1,6 +1,8 @@
-name = "Progressbar Meme"
-shortname = "Meme"
-prolevel = 30
-unlocklevel = 30
-requiredsystem = "98"
-requiredstring = "PB98"
+class system():
+    name = "Progressbar Meme"
+    shortname = "Meme"
+    prolevel = 30
+    unlocklevel = 30
+    systemunlock = "2000"
+    requiredstring = "PB98"
+    listinbootmenu = 3
