5d8f1a00b98c2833262d15ac46a3ccfee16eecc3
BurningInfern0 <epiclawl46@gmail.com>
2022-04-24 20:07:23 -0400

    upload language files (non-en_US files are placeholders)

---
+++ lang/ro_RO.py
@@ -0,0 +1,59 @@
+class language():
+  # ro_RO language configuration file
+  # {*} <-- these are for python's .format() command, used for placing variables in strings.
+  # Please do not remove \n, {*}, or [] objects.
+
+  # boot
+  pleaseSelect = 'Vă rugăm să selectați limba dumneavoastră prin introducerea codului de limbă din stânga.'
+  sparrow = '[white]Sparrow Assistant Enhanced Text BIOS.[not bold]80.1[/not bold][/white] - [bright_yellow]Energy Star (un)Powered[/bright_yellow]'
+  # {0} is for the build verson, {1} is for the compile date.
+  version = '[white]CLI ver. [bold]{0}[/bold] - compiled {1}[/white]'
+  dev = '[bold red]- DEVELOPMENT BUILD // BUGS MAY BE COMMON -[/bold red]\n\n'
+  # {0} is for the boot menu list number, {1} is for the full system name, {2} is the level number required for unlock, {3} is the level you need in a specified system to unlock this one.
+  notUnlocked = '[red][not bold]{0}[/not bold]. [not bold]{1}[/not bold] - Get to level {2} in {3} to unlock this![/red]'
+  credits1 = "Progress[#12cc00]CLI[/#12cc00]95 [blink]Development[/blink] and [blink]Contributor[/blink] Team"
+  credits2 = "Progress[#12cc00]CLI[/#12cc00]95 [blink]Translation[/blink] Team"
+
+  # player
+  loading = '\n\n\nNow Loading...'
+  bar = '\nYour bar:'
+  barProgressP1 = "\nYou have"
+  barProgressP2 = "% with"
+  barProgressP3 = "% orange in your progressbar."
+  inYourBar = "in your progressbar"
+  settings = '╔════════════════════════╗\n║     S e t t i n g      ║\n║    1 - Popup           ║\n║    2 - Return          ║\n╚════════════════════════╝\n'
+  doYouWant = "Do you want to have this :"
+  popupSetting = "\nto the popup window? (Y/N)"
+  wait = 'P l e a s e  w a i t . . .\n\n\n'
+  closeCMD = '[bold yellow]It is now safe to close your Command Line Interface.[/bold yellow]'
+  bm1 = 'B e g i n  M e n u'
+  bm2 = 'New Game'
+  bm3 = 'Load Game'
+  bm4 = 'Restart'
+  bm5 = 'Shutdown'
+  bm6 = 'Settings'
+  bm7 = 'Resume'
+  annoyingPopup = ':) Annoying popup!'
+  outOfLives = "[bold bright_blue]You are out of lives. Game over![/bold bright_blue]"
+  noLevelTaken = '[i]A level has not been taken.[/i]'
+  negateLevel = '[bold i]-1 Level[/bold i]'
+  level = 'Level'
+  oneLifeLeft = "You have [italic bright_red]1 life left[/italic bright_red]. Be careful."
+  # {0} is the current number of lives.
+  livesLeft = "You have {0} lives left."
+  pressInstructions = "Type 'c' to catch, any other key to move away, and 'q' to quit.\n"
+  gameOver = 'Game Over! Thanks for playing!'
+  pressEnter = '\nPress ENTER to play another level.'
+  gameBravo = 'Bravo!'
+  gamePerfect = 'Perfect!'
+  gameOuterSpace = 'Outer space!'
+  gameYinAndYang = 'Yin and yang'
+  gameNonconformist = "Nonconformist!"
+  newSystem = '[i]A new system has unlocked...[/i]'
+  proCongrats = '\nCongratulations! You are the Professional!'
+  proAcquired = 'Pro Label acquired!'
+  expertAcquired = '\nExpert Label acquired!'
+  masterAcquired = '\nMaster Label acquired!'
+  adeptAcquired = '\nAdept Label acquired!'
+  grandAcquired = '\nGrand Label acquired!'
+  whatAcquired = '\nWhat?'
