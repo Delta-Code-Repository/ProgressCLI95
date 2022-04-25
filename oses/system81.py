e613e4f6407444f45da9110acf6ad78d80bec0d6
SevenSixteen <83654349+poggers16@users.noreply.github.com>
2022-04-25 22:22:11 +0700

    frick

---
+++ oses/system81.py
@@ -0,0 +1,9 @@
+class system():
+    name = "Progressbar 81"
+    shortname = "81"
+    prolevel = 50
+    unlocklevel = 60
+    requiredstring = "PB7"
+    systemunlock = "10"
+    listinbootmenu = 8
+    startupstring = "8 1"
