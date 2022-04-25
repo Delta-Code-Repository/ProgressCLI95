96b49faaa1e2ffe350f9302564d96c44bd5afef2
SevenSixteen <83654349+poggers16@users.noreply.github.com>
2022-04-25 22:21:35 +0700

    yeet

--- system7.py
+++
@@ -1,9 +0,0 @@
-class system():
-    name = "Progressbar 7"
-    shortname = "7"
-    prolevel = 40
-    unlocklevel = 50
-    requiredstring = "PBWista"
-    systemunlock = "81"
-    listinbootmenu = 7
-    startupstring = "7"
