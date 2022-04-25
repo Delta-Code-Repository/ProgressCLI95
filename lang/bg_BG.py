d2528e7b7d0d4acf104ab0d698382956e2571a48
Mark <markverb4@gmail.com>
2022-04-25 14:05:23 +0300

    why did i translate CLI

--- lang/bg_BG.py
+++ lang/bg_BG.py
@@ -8,7 +8,7 @@ class language():
   pleaseSelect = 'Моля селектирайте вашият език като напишете кода отляво'
   sparrow = '[white]Спароу асистент подобрен текст BIOS.[not bold]80.1[/not bold][/white] - [bright_yellow]Енергийна Звезда (не)Захранено[/bright_yellow]'
   # {0} is for the build verson, {1} is for the compile date.
-  version = '[white]КЛИ версия [bold]{0}[/bold] - компилиран {1}[/white]'
+  version = '[white]CLI версия [bold]{0}[/bold] - компилиран {1}[/white]'
   dev = '[bold red]- ВЕРСИЯ РАЗРАБОТКА // БЪГОВЕ МОЖЕ ДА СА ЧЕСТИ -[/bold red]\n\n'
   # {0} is for the boot menu list number, {1} is for the full system name, {2} is the level number required for unlock, {3} is the level you need in a specified system to unlock this one.
   notUnlocked = '[red][not bold]{0}[/not bold]. [not bold]{1}[/not bold] - Стигнете до ниво {2} в {3} за да отключите това![/red]'
