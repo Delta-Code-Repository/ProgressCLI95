59d5c3a1dc53d6b4f53e78a5e4a9ebe7f33aa924
SevenSixteen <83654349+poggers16@users.noreply.github.com>
2022-04-25 22:22:33 +0700

    yeet

--- system81.py
+++
@@ -1,9 +0,0 @@
-class system():
-    name = "Progressbar 81"
-    shortname = "81"
-    prolevel = 50
-    unlocklevel = 60
-    requiredstring = "PB7"
-    systemunlock = "10"
-    listinbootmenu = 8
-    startupstring = "8 1"
