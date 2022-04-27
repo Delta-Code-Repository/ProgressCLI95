f11efb5be7748eaf034baea04cf11c996d224755
markverb1 <77983517+markverb1@users.noreply.github.com>
2022-04-27 17:01:32 +0300

    Update bg_BG.py

--- lang/bg_BG.py
+++ lang/bg_BG.py
@@ -9,7 +9,7 @@ class language():
   sparrow = '[white]Спароу асистент подобрен текст BIOS.[not bold]80.1[/not bold][/white] - [bright_yellow]Енергийна Звезда (не)Захранено[/bright_yellow]'
   # {0} is for the build verson, {1} is for the compile date.
   version = '[white]CLI версия [bold]{0}[/bold] - компилиран {1}[/white]'
-  dev = '[bold red]- ВЕРСИЯ РАЗРАБОТКА // БЪГОВЕ МОЖЕ ДА СА ЧЕСТИ -[/bold red]\n\n'
+  dev = '[bold red]- ВЕРСИЯ РАЗРАБОТКА // БЪГОВЕ МОЖЕ ДА СА ЧЕСТО СРЕЩАНИ -[/bold red]\n\n'
   # {0} is for the boot menu list number, {1} is for the full system name, {2} is the level number required for unlock, {3} is the level you need in a specified system to unlock this one.
   notUnlocked = '[red][not bold]{0}[/not bold]. [not bold]{1}[/not bold] - Стигнете до ниво {2} в {3} за да отключите това![/red]'
   credits1 = "Екип от [blink]програмисти[/blink]"
