0a1e7d7daba7ea01f183d3bd5fec13bab31bca8b
5jiji <60753482+5jiji@users.noreply.github.com>
2022-07-07 13:51:43 +0200

    Updated the russian language

--- lang/ru_RU.py
+++ lang/ru_RU.py
@@ -1,27 +1,27 @@
 class language():
-  # en_US/ru_RUv2.1 Файл конфигурацыи языка -C
+  # en_US/ru_RUv2.1.2 Файл конфигурации языка -C
   #{*} <-- это для команды python .format(), используемый для размещения переменных в строках кода.
   # Пж не удаляйте эти (\n, {*}, []) объекты. -O
 
-  # Загрузка -T
-  pleaseSelect = 'Пожалуйста, введите код вашего языка.'
+  # Файл boot -T
+  pleaseSelect = 'Пожалуйста, выберите язык.'
   sparrow = '[white]Sparrow Assistant Enhanced Text BIOS.[not bold]80.1[/not bold][/white] - [bright_yellow]Energy Star (un)Powered[/bright_yellow]'
   # {0} <-- Версия Билда, {1} <-- Дата выпуска.
-  version = '[white]CLI версии [bold]{0}[/bold] - скомпилированной {1}[/white]'
-  dev = '[bold red]- БИЛД В РАЗРАБОТКЕ // ВОЗМОЖНЫ бАГИ -[/bold red]\n\n'
+  version = '[white]ProgressCLI95 версии [bold]{0}[/bold] - скомпилированной {1}[/white]'
+  dev = '[bold red]- БИЛД В РАЗРАБОТКЕ // ВОЗМОЖНЫ БАГИ -[/bold red]\n\n'
   # {0} - номер в списке меню загрузки, {1} - полное имя системы, {2} - кл-во уровней, необходимых для разблокировки, {3} - уровень, который необходимо получить в указанной системе для разблокировки этой системы. -T
   notUnlocked = '[red][not bold]{0}[/not bold]. [not bold]{1}[/not bold] - Получите {2} уровень в {3} чтобы разблокировать![/red]'
   credits1 = "[blink]Команда Разработчиков[/blink] и [blink]Участников Проекта[/blink] Progress[#12cc00]CLI[/#12cc00]95"
   credits2 = "Команда [blink]Переводчиков[/blink] Progress[#12cc00]CLI[/#12cc00]95"
 
-  # player -Y
+  #Файл player -Y
   loading = '\n\n\nСейчас загружается...'
   bar = '\nВаш прогрессбар:'
   barProgress1 = '\nУ вас {0}% с {1}% оранжевых сегментов в вашем прогрессбаре.'
   barProgress2 = "\nУ вас {0}% в вашем прогрессбаре."
   doYouWant = "Вы бы хотели видеть это:"
   popupSetting = "\nв всплывающем окне? (Y да/N нет)"
-  wait = 'П о ж а л у й с т а  п о д о ж д и т е . . .\n\n\n'
+  wait = 'П о ж а л у й с т а   п о д о ж д и т е . . .\n\n\n'
   closeCMD = '[bold yellow]Теперь можно безопасно закрыть Интерфейc Командной Строки.[/bold yellow]'
   bm1 = 'З а п у с к'
   bm2 = 'Новая Игра'
@@ -52,10 +52,15 @@ class language():
   gamePoints = "очков"
   newSystem = '[i]Новая система разблокирована...[/i]'
   proCongrats = '\nПоздравляю! Вы теперь Профессионал!'
-  proAcquired = '\nПро бэджик получен!'
-  expertAcquired = '\nЭксперт бэджик получен!'
-  masterAcquired = '\nМастер бэджик получен!'
-  adeptAcquired = '\nАдепт бэджик получен!'
-  grandAcquired = '\nГранд бэджик получен!'
+  proAcquired = '\nПро бейджик получен!'
+  expertAcquired = '\nЭксперт бейджик получен!'
+  masterAcquired = '\nМастер бейджик получен!'
+  adeptAcquired = '\nАдепт бейджик получен!'
+  grandAcquired = '\nГранд бейджик получен!'
   whatAcquired = '\nЧТА?'
   magicPink = 'Магический розовый!'
+  selectos = 'Введите название системы в которую хотите играть : BarOS или Progressbar'
+  notright = 'Введите точно также как написано : BarOS или Progressbar'
+  osselect = 'Выберите BarOS или Progressbar'
+  youcan = 'Если хотите выйти, напишите "exit"'
+  chngtrst = 'Или ещё введите "chlang" если хотите изменить язык'
