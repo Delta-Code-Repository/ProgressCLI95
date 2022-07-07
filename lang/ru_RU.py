80c687fecea785e7f3dad7e3f56e71b042e5c74d
5jiji <60753482+5jiji@users.noreply.github.com>
2022-07-07 11:45:18 +0200

    Added Russian, made by H8ther

---
+++ lang/ru_RU.py
@@ -0,0 +1,61 @@
+class language():
+  # en_US/ru_RUv2.1 Файл конфигурацыи языка -C
+  #{*} <-- это для команды python .format(), используемый для размещения переменных в строках кода.
+  # Пж не удаляйте эти (\n, {*}, []) объекты. -O
+
+  # Загрузка -T
+  pleaseSelect = 'Пожалуйста, введите код вашего языка.'
+  sparrow = '[white]Sparrow Assistant Enhanced Text BIOS.[not bold]80.1[/not bold][/white] - [bright_yellow]Energy Star (un)Powered[/bright_yellow]'
+  # {0} <-- Версия Билда, {1} <-- Дата выпуска.
+  version = '[white]CLI версии [bold]{0}[/bold] - скомпилированной {1}[/white]'
+  dev = '[bold red]- БИЛД В РАЗРАБОТКЕ // ВОЗМОЖНЫ бАГИ -[/bold red]\n\n'
+  # {0} - номер в списке меню загрузки, {1} - полное имя системы, {2} - кл-во уровней, необходимых для разблокировки, {3} - уровень, который необходимо получить в указанной системе для разблокировки этой системы. -T
+  notUnlocked = '[red][not bold]{0}[/not bold]. [not bold]{1}[/not bold] - Получите {2} уровень в {3} чтобы разблокировать![/red]'
+  credits1 = "[blink]Команда Разработчиков[/blink] и [blink]Участников Проекта[/blink] Progress[#12cc00]CLI[/#12cc00]95"
+  credits2 = "Команда [blink]Переводчиков[/blink] Progress[#12cc00]CLI[/#12cc00]95"
+
+  # player -Y
+  loading = '\n\n\nСейчас загружается...'
+  bar = '\nВаш прогрессбар:'
+  barProgress1 = '\nУ вас {0}% с {1}% оранжевых сегментов в вашем прогрессбаре.'
+  barProgress2 = "\nУ вас {0}% в вашем прогрессбаре."
+  doYouWant = "Вы бы хотели видеть это:"
+  popupSetting = "\nв всплывающем окне? (Y да/N нет)"
+  wait = 'П о ж а л у й с т а  п о д о ж д и т е . . .\n\n\n'
+  closeCMD = '[bold yellow]Теперь можно безопасно закрыть Интерфейc Командной Строки.[/bold yellow]'
+  bm1 = 'З а п у с к'
+  bm2 = 'Новая Игра'
+  bm3 = 'Загрузить'
+  bm4 = 'Перезагрузить'
+  bm5 = 'Выключить'
+  bm6 = 'Настройки'
+  bm7 = 'Продолжить'
+  sett1 = 'Н а с т р о й к и'
+  sett2 = 'Попап'
+  sett3 = 'Вернуться'
+  annoyingPopup = ':) Дурацкий попап!'
+  outOfLives = "[bold bright_blue]Вы израсходовали все свои жизни. Игра окончена![/bold bright_blue]"
+  noLevelTaken = '[i]Уровень не получен.[/i]'
+  negateLevel = '[bold i]-1 Уровень[/bold i]'
+  level = 'Уровень'
+  oneLifeLeft = "У вас осталось [italic bright_red]1 жизнь[/italic bright_red]. Будьте осторожней."
+  # {0} <-- Число ваших жизней. -3
+  livesLeft = "У вас осталось {0} жизни."
+  pressInstructions = "Введите 'c' чтобы поймать сегмент, любой другой клавишей увернуться, а 'q' для выхода.\n"
+  gameOver = 'Игра окончена! Спасибо за игру!'
+  pressEnter = '\nНажмите ENTER чтобы сыграть следующий уровень.'
+  gameBravo = 'Браво!'
+  gamePerfect = 'Идеально!'
+  gameOuterSpace = 'Заграница!'
+  gameYinAndYang = 'Инь-яь'
+  gameNonconformist = "Нонконформист!"
+  gamePoints = "очков"
+  newSystem = '[i]Новая система разблокирована...[/i]'
+  proCongrats = '\nПоздравляю! Вы теперь Профессионал!'
+  proAcquired = '\nПро бэджик получен!'
+  expertAcquired = '\nЭксперт бэджик получен!'
+  masterAcquired = '\nМастер бэджик получен!'
+  adeptAcquired = '\nАдепт бэджик получен!'
+  grandAcquired = '\nГранд бэджик получен!'
+  whatAcquired = '\nЧТА?'
+  magicPink = 'Магический розовый!'
