91525e75bd6db2b01fed8d8a3f1b2ac35f8bc7fc
leapofazzam123 <leapofazzam@gmail.com>
2022-07-11 12:41:40 +0700

    finished the lolcat language

--- lang/en_LOL.py
+++ lang/en_LOL.py
@@ -5,44 +5,44 @@ class language():
 
   # boot
   pleaseSelect = 'PLZ SELECT UR LANGUAGE BY ENTERIN TEH LANGUAGE CODE 2 TEH LEFT.'
-  sparrow = '[white]Sparrow Assistant Enhanced Text BIOS.[not bold]80.1[/not bold][/white] - [bright_yellow]Energy Star (un)Powered[/bright_yellow]'
+  sparrow = '[white]SPARROW ASISTANT ENHANCD TEKS BIOZ[not bold]80.1[/not bold][/white] - [bright_yellow]ENERGI STAR (UN)POWERD[/bright_yellow]'
   # {0} is for the build verson, {1} is for the compile date.
-  version = '[white]CLI ver. [bold]{0}[/bold] - compiled {1}[/white]'
-  dev = '[bold red]- DEVELOPMENT BUILD // BUGS MAY BE COMMON -[/bold red]\n\n'
+  version = '[white]CLI VER. [bold]{0}[/bold] - COMPILD {1}[/white]'
+  dev = '[bold red]- DEVELOPMNT BUILD // BUGZ CUD BE COMMON -[/bold red]\n\n'
   # {0} is for the boot menu list number, {1} is for the full system name, {2} is the level number required for unlock, {3} is the level you need in a specified system to unlock this one.
   notUnlocked = '[red][not bold]{0}[/not bold]. [not bold]{1}[/not bold] - GIT 2 LEVEL {2} IN {3} 2 UNLOCK DIS![/red]'
-  credits1 = "Progress[#12cc00]CLI[/#12cc00]95 [blink]Development[/blink] and [blink]Contributor[/blink] Team"
-  credits2 = "Progress[#12cc00]CLI[/#12cc00]95 [blink]Translation[/blink] Team"
+  credits1 = "PROGRES[#12cc00]CLI[/#12cc00]95 [blink]DEVELOPMNT[/blink] and [blink]CONTRIBUTR[/blink] TIM"
+  credits2 = "PROGRES[#12cc00]CLI[/#12cc00]95 [blink]TRANZLASHUN[/blink] TIM"
 
   # player
-  loading = '\n\n\nNAO LOADIN...'
-  bar = '\nYour bar:'
+  loading = '\n\n\nNAW LOADIN...'
+  bar = '\nUR BAR:'
   barProgress1 = '\nU HAS {0}% WIF {1}% ORANGE IN UR PROGRESBAR.'
   barProgress2 = "\nU HAS {0}% IN UR PROGRESBAR."
   doYouWant = "DO U WANTS 2 HAS DIS :"
   popupSetting = "\n2 TEH POPUP WINDOW? (Y/N)"
-  wait = 'P l s  w a i t . . .\n\n\n'
-  closeCMD = '[bold yellow]It is now safe to close your Command Line Interface.[/bold yellow]'
-  bm1 = 'B e g i n  M e n u'
-  bm2 = 'New Game'
-  bm3 = 'Load Game'
-  bm4 = 'Restart'
-  bm5 = 'Shutdown'
-  bm6 = 'Settings'
-  bm7 = 'Resume'
-  sett1 = 'S e t t i n g s'
-  sett2 = 'Popup'
-  sett3 = 'Return'
-  annoyingPopup = ':) Annoyin popup!'
-  outOfLives = "[bold bright_blue]U R OUT OV LIVEZ. GAME OVAR![/bold bright_blue]"
-  noLevelTaken = '[i]A level has not been taken.[/i]'
-  negateLevel = '[bold i]-1 Level[/bold i]'
-  level = 'Level'
-  oneLifeLeft = "U HAS [ITALIC BRIGHT_RED] 1 LIFE LEFT [/ITALIC BRIGHT_RED]. BE CAREFUL."
+  wait = 'P L Z  W A I T . . .\n\n\n'
+  closeCMD = '[bold yellow]IT NAW SAFE 2 CLOSE UR COMMAN LINE INTERFEZ/bold yellow]'
+  bm1 = 'B I G I N  M E N U'
+  bm2 = 'NEW GAM'
+  bm3 = 'LOAD GAM'
+  bm4 = 'RESTART'
+  bm5 = 'SHUTDOWN'
+  bm6 = 'SETTINGZ'
+  bm7 = 'RESUME'
+  sett1 = 'S E T T I N G Z'
+  sett2 = 'POPUP'
+  sett3 = 'RETURN'
+  annoyingPopup = ':) ANOYIN POPUP!'
+  outOfLives = "[bold bright_blue]U R OUT OV LIFEZ. GAM OVAR![/bold bright_blue]"
+  noLevelTaken = '[i]A LEVEL HAZ NOT TAKEN[/i]'
+  negateLevel = '[bold i]-1 LEVEL[/bold i]'
+  level = 'LEVEL'
+  oneLifeLeft = "U HAS [italic bright_red] 1 LIFE LEFT [/italic bright_red]. BE CERFUL."
   # {0} is the current number of lives.
-  livesLeft = "U HAS {0} LIVES LEFT."
+  livesLeft = "U HAS {0} LIFEZ LEFT."
   pressInstructions = "TYPE 'C' 2 KATCH, ANY OTHR KEY 2 MOOV AWAY, AN 'Q' 2 QUIT.\n"
-  gameOver = 'GAME OVAR! THX 4 PLAYIN!'
+  gameOver = 'GAM OVAR! THX 4 PLAYIN!'
   pressEnter = '\nPRES ENTR 2 PULAY ANOTHR LEVEL.'
   gameBravo = 'BRAVO!'
   gamePerfect = 'PERFIK!'
@@ -54,8 +54,8 @@ class language():
   proCongrats = '\nCONGRATULASHUNS! U R TEH PROFESHUNAL!!'
   proAcquired = 'PRO LABEL ACQUIRD!'
   expertAcquired = '\nEXPERT LABEL ACQUIRD!'
-  masterAcquired = '\nMASTER LABEL ACQUIRD!'
+  masterAcquired = '\nMASTEAH LABEL ACQUIRD!'
   adeptAcquired = '\nADEPT LABEL ACQUIRD!'
   grandAcquired = '\nGRAND LABEL ACQUIRD!'
-  whatAcquired = '\nWHAT?'
+  whatAcquired = '\nWUT?'
   magicPink = 'MAGIC PINK!'
